939cab1c5dc969a39e6365c15373bb5dcd404bbb
HItanshu Kaushik <118196801+Hitanshu-09@users.noreply.github.com>
2024-11-08 09:04:39 +0530

    Add files via upload

---
+++ grocery.sql
@@ -0,0 +1,3 @@
+-- ALTER TABLE `supermart grocery sales - retail analytics dataset` RENAME COLUMN `ï»¿Order ID` TO Order_id;--
+-- SELECT distinct `Order Date` from `supermart grocery sales - retail analytics dataset`;--
+Select date_format(`Order Date`) from `supermart grocery sales - retail analytics dataset`;
